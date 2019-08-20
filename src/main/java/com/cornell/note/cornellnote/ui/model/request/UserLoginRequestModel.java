ee2f507682686729cfc2af53db7b864e7ab64eca
singuye <simon_nguyen@gap.comls>
2019-08-19 22:36:15 -0700

    add user login request model

---
+++ src/main/java/com/cornell/note/cornellnote/ui/model/request/UserLoginRequestModel.java
@@ -0,0 +1,11 @@
+package com.cornell.note.cornellnote.ui.model.request;
+
+import lombok.Data;
+
+@Data
+public class UserLoginRequestModel {
+
+    private String email;
+    private String password;
+
+}
